c05d65b36a2be746318a0bfa681eadd772b95b36
Wevison Ramalho Silva <wevisonramalho@gmail.com>
2022-03-29 21:38:37 -0300

    [ FEATURE ] Adding project in production.

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'), ['komny+Ffv3+DxzvwhXkbmQ==','EIDjvfsBpIcvneRCaukFGg==','Gl6FLars8cw4aFtbClfDTw==','L7ZTLg/2pbBTrgszW0oRfw=='],
+    keys: env.array("APP_KEYS"),
   },
 });
