8ec97d18e7f82b357ca96fe419bffcc27fd1dfd3
Wevison Ramalho Silva <wevisonramalho@gmail.com>
2022-03-30 10:23:33 -0300

    Update database config

--- config/server.js
+++ config/server.js
@@ -2,6 +2,11 @@ module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
   app: {
-    keys: env.array("APP_KEYS"),
+    keys: env.array("APP_KEYS", [
+      "komny+Ffv3+DxzvwhXkbmQ==",
+      "EIDjvfsBpIcvneRCaukFGg==",
+      "Gl6FLars8cw4aFtbClfDTw==",
+      "L7ZTLg/2pbBTrgszW0oRfw==",
+    ]),
   },
 });
